8b9cce8fc987fd1c2f7776e9de2ecf66df46b0f1
isabia <isabellabeatrizn@outlook.com>
2020-02-19 13:23:22 -0300

    Start

---
+++ PokerApp/PokerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
