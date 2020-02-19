7615e399c227163e02f4b30e62f9012faa75043e
isabia <isabellabeatrizn@outlook.com>
2020-02-19 17:00:55 -0300

    Adição classe baralho

---
+++ PokerApp/PokerApp/Baralho.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+
+namespace PokerApp
+{
+    class Baralho : Carta
+    {
+        
+            const int QUANTIDADE_DE_CARTAS = 52; 
+            private Carta[] _baralho; 
+
+            public Baralho() => _baralho = new Carta[QUANTIDADE_DE_CARTAS];
+            
+            public Carta[] getBaralho { get { return _baralho; } } 
+
+          
+            public void setBaralho()
+            {
+                int i = 0;
+                foreach (Naipe n in Enum.GetValues(typeof(Naipe)))
+                {
+                    foreach (Valor v in Enum.GetValues(typeof(Valor)))
+                    {
+                        _baralho[i] = new Carta { NaipeUsado = n, ValorUsado = v };
+                        i++;
+                    }
+                }
+
+            }
+
+           
+    }
+    
+}
