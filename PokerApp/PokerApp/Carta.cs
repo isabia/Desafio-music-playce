39043e10ba57831fa8a798374f805b1c3cbbf4d5
isabia <isabellabeatrizn@outlook.com>
2020-02-19 16:44:40 -0300

    Criação classe Carta

---
+++ PokerApp/PokerApp/Carta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerApp
+{
+    class Carta
+    {
+        public enum Naipe
+        {
+            E=1, O, C, P
+        }
+
+        public enum Valor
+        {
+            DOIS = 2, TRES, QUATRO, CINCO, SEIS, SETE, OITO,
+            NOVE, DEZ, VALETE, DAMA, REI, AIS
+        }
+
+        public Naipe NaipeUsado { get; set; }
+        public Valor ValorUsado { get; set; }
+    }
+}
