14533796d87325901dbbc31a7e96af34e20e64cf
isabia <isabellabeatrizn@outlook.com>
2020-02-19 18:19:47 -0300

    Start point criado

--- PokerApp/PokerApp/Program.cs
+++ PokerApp/PokerApp/Program.cs
@@ -6,7 +6,32 @@ namespace PokerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(65, 40);
+           
+            Console.Title = "Poker Game";
+            GerenciadorDasCartas gerenciamentoDasCartas = new GerenciadorDasCartas();
+            bool desistir = false;
+
+            while (!desistir)
+            {
+                gerenciamentoDasCartas.Deal();
+
+                char selection = ' ';
+                while (!selection.Equals('S') && !selection.Equals('N'))
+                {
+                    Console.WriteLine("Jogar de novo?");
+                    selection = Convert.ToChar(Console.ReadLine().ToUpper());
+
+                    if (selection.Equals('S'))
+                        desistir = false;
+                    else if (selection.Equals('N'))
+                        desistir = true;
+                    else
+                        Console.WriteLine("Seleção inválida ¯\\(°_o)/¯");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
